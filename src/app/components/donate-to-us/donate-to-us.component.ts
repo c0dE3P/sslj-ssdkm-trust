bb64f2934b1e9fe57de3ac849ab088a3c1a9cdca
Deep Kar Sarkar <deepksarkar11@gmail.com>
2023-06-25 16:44:29 +0530

    First Commit

---
+++ src/app/components/donate-to-us/donate-to-us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donate-to-us',
+  templateUrl: './donate-to-us.component.html',
+  styleUrls: ['./donate-to-us.component.scss']
+})
+export class DonateToUsComponent {
+
+}
